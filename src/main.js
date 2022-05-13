5bb9040e0b3e890c1885843fe992c617698208d4
zahra sayahi <zahraaa.sty@gmail.com>
2022-05-13 20:34:08 +0430

    bootstrap added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import "bootstrap/dist/css/bootstrap.css"
 import App from './App.vue'
 import router from './router'
 import store from './store/index.js'
@@ -6,3 +7,4 @@ import store from './store/index.js'
 import './assets/main.css'
 
 createApp(App).use(router).use(store).mount('#app')
+import "bootstrap/dist/js/bootstrap.js"
